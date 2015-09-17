cb8d3c5e7cf1bd7fba4fe53b875f3e549d56b4c2
jakezehring <jakezehring@comcast.net>
2015-09-17 10:12:14 -0700

    Added character tab

--- www/js/app.js
+++ www/js/app.js
@@ -67,6 +67,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'PlaylistCtrl'
       }
     }
+  })
+
+  .state('app.character', {
+      url: '/character',
+      views: {
+          'menuContent': {
+              templateUrl: 'templates/playlist.html',
+              controller: 'PlaylistCtrl'
+          }
+      }
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
