7160e4fc22011ff2949d8f726594d9a62e01bd49
Nicholas Olson <nicholas.olson@build.com>
2015-09-21 02:50:18 -0700

    Added bare bones abilities partial

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,41 +14,44 @@ angular.module('starter.controllers', [])
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
+	scope: $scope
   }).then(function(modal) {
-    $scope.modal = modal;
+	$scope.modal = modal;
   });
 
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
-    $scope.modal.hide();
+	$scope.modal.hide();
   };
 
   // Open the login modal
   $scope.login = function() {
-    $scope.modal.show();
+	$scope.modal.show();
   };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+	console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+	// Simulate a login delay. Remove this and replace with your login
+	// code if using a login system
+	$timeout(function() {
+	  $scope.closeLogin();
+	}, 1000);
   };
+})
+
+.controller('AbilitiesCtrl', function($scope) {
 })
 
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+	{ title: 'Reggae', id: 1 },
+	{ title: 'Chill', id: 2 },
+	{ title: 'Dubstep', id: 3 },
+	{ title: 'Indie', id: 4 },
+	{ title: 'Rap', id: 5 },
+	{ title: 'Cowbell', id: 6 }
   ];
 })
 
