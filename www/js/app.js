1e673ce5e398e2f4553df433ffae5894f417741b
jakezehring <jakezehring@comcast.net>
2015-09-23 14:22:19 -0700

    Fixed merge conflicts with feats

--- www/js/app.js
+++ www/js/app.js
@@ -7,76 +7,85 @@
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-   // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+    $stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+      .state('app', {
+          url: '/app',
+          abstract: true,
+          templateUrl: 'templates/menu.html',
+          controller: 'AppCtrl'
+      })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
+    .state('app.search', {
+        url: '/search',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/search.html'
+            }
         }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlists.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  })
-  .state('app.feats', {
-    url: '/feats',
-    views: {
-      'menuContent': {
-	 templateUrl: 'templates/feats.html',
-	 controller: 'FeatsCtrl'
-      }
-    }
-  });
+    .state('app.browse', {
+        url: '/browse',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/browse.html'
+            }
+        }
+    })
+      .state('app.playlists', {
+          url: '/playlists',
+          views: {
+              'menuContent': {
+                  templateUrl: 'templates/playlists.html',
+                  controller: 'PlaylistsCtrl'
+              }
+          }
+      })
+
+    .state('app.single', {
+        url: '/playlists/:playlistId',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/playlists.html',
+                controller: 'PlaylistCtrl'
+            }
+        }
+    })
+    .state('app.feats', {
+        url: '/feats',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/feats.html',
+                controller: 'FeatsCtrl'
+            }
+        }
+    })
+    .state('app.character', {
+        url: '/character',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/character.html',
+                controller: 'CharacterCtrl'
+            }
+        }
+});
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
 });
