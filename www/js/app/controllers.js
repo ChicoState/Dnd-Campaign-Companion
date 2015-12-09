6eb10e4f39e895b239ac77525ea6d14b386914f0
jakezehring <jakezehring@comcast.net>
2015-12-09 15:16:59 -0800

    Loads first 8 values from parse

--- www/js/app/controllers.js
+++ www/js/app/controllers.js
@@ -46,10 +46,31 @@ angular.module('app.controllers', [])
         '$state', '$scope', 'UserService',   // <-- controller dependencies
         function ($state, $scope, UserService) {
 
-            debugger;
+            $scope.combat = [];
             UserService.currentUser().then(function (_user) {
                 $scope.user = _user;
-            });
+                var combat = Parse.Object.extend("Combat");
+                var query = new Parse.Query(combat);
 
+                query.equalTo("username", $scope.user.attributes.username)
+
+                query.find({
+                    success: function (results){
+                        var object = results[0];
+                        console.log(object)
+                        $scope.combat.push({
+                            HP: object.attributes.HP,
+                            AC: object.attributes.AC,
+                            Init: object.attributes.initiative,
+                            Fort: object.attributes.Fortitude,
+                            Reflex: object.attributes.Reflex,
+                            Will: object.attributes.Will,
+                            BaseA: object.attributes.BaseAttack,
+                            Grapple: object.attributes.Grapple
+                        });
+                        console.log($scope.combat[0]);
+                    }
+                });
+            });
 
         }]);
