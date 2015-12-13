2f2f70e47f4ad6c24aaf24d7b081e168c9515506
Jacob Shouse <lordofjam@charter.net>
2015-12-12 21:07:49 -0800

    fixed stuff and included money to the tab

--- www/js/app/controllers.js
+++ www/js/app/controllers.js
@@ -43,44 +43,58 @@ angular.module('app.controllers', [])
 
     .controller('InventoryCtrl', [
         '$state', '$scope', 'UserService', '$ionicPopup',
-    function ($state, $scope, UserService, $ionicPopup){
-        UserService.currentUser().then(function(_user) {
-            $scope.user = _user;
-        });
-        $scope.items = [];
-        $scope.gear = [];
-        
-        $scope.abiladd = function(type) {
-            $scope.data = {type}
-            var mypop =
-            $ionicPopup.show({
-                template: 'Title: <input type="text" ng-model="data.title"> <br> Description: <textarea name="desc" cols="40" rows="5" maxlength="200" ng-model="data.description" style="height: 130px; min-height:130px; max-height:130px;">',
-                title: 'Add Item',
-                cssClass: 'additem',
-                scope: $scope,
-                buttons: [
-                {text: 'Cancel' },
-                {text: 'Save',
-                 type: 'button-positive',
-                 onTap: function(e) {
-                    if((!$scope.data.title)||(!$scope.data.description)){
-                        e.preventDefault();
+   function ($state, $scope, UserService, $ionicPopup){
+       UserService.currentUser().then(function(_user) {
+           $scope.user = _user;
+       });
+       $scope.doLogoutAction = function () {
+                UserService.logout().then(function () {
+
+                    // transition to next state
+                    $state.go('app-login');
+
+                }, function (_error) {
+                    alert("error logging in " + _error.debug);
+                })
+            };
+       $scope.items = [];
+       $scope.gear = [];
+       
+       
+       $scope.shouldShowDelete = true;
+       $scope.listCanSwipe = true; 
+       
+       $scope.itemadd = function(type) {
+           $scope.data = {type}
+           var mypop =
+           $ionicPopup.show({
+               template: 'Title: <input type="text" ng-model="data.title"> <br> Description: <textarea name="desc" cols="40" rows="5" maxlength="200" ng-model="data.description" style="height: 130px; min-height:130px; max-height:130px;">',
+               title: 'Add Item',
+               cssClass: 'addAbil',
+               scope: $scope,
+               buttons: [
+               {text: 'Cancel' },
+               {text: 'Save',
+                type: 'button-positive',
+                onTap: function(e) {
+                   if((!$scope.data.title)||(!$scope.data.description)){
+                       e.preventDefault();
                     } else{
-                        return $scope.data;
-                    }
-                    }
-                },
-                ]
-            });
-            mypop.then(function(res){
-                if(res){
-                if(res.type == 1){
-                    $scope.items.push(res);
-                } else if(res.type==2){
-                    $scope.gear.push(res);
-                }
-                }
-            });
-        };
-    }]
+                      return $scope.data;
+                   }
+                   }
+               },
+               ]
+           });
+           mypop.then(function(res){
+               if(res){
+               if(res.type == 1){
+                   $scope.items.push(res);
+               } else if(res.type==2){
+                   $scope.gear.push(res);
+               } 
+               }   
+           });
+       };
+   }]
 );
