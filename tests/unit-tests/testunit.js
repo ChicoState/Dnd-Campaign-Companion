186594f6b6a75a5ab95e3d2ebbba8ea0404209e7
Nicholas Olson <nicholas.olson@build.com>
2015-12-14 02:39:42 -0800

    end to end tests added, all passing

--- tests/unit-tests/testunit.js
+++ tests/unit-tests/testunit.js
@@ -2,34 +2,71 @@ describe('LoginController', function() {
 
     var controller, 
         deferredLogin,
-        UserServiceMock,
+        userServiceMock,
         stateMock,
         ionicPopupMock;
 
-    // TODO: Load the App Module
-
-    // TODO: Instantiate the Controller and Mocks
-
+    // load the module for our app
+    beforeEach(module('starter'));
+    
+    // disable template caching
+    beforeEach(module(function($provide, $urlRouterProvider) {
+        $provide.value('$ionicTemplateCache', function(){} );
+        $urlRouterProvider.deferIntercept();
+    }));
+    
+    // instantiate the controller and mocks for every test
+    beforeEach(inject(function($controller, $q) {
+        deferredLogin = $q.defer();
+        
+        // mock userService
+        userServiceMock = {
+            login: jasmine.createSpy('login spy')
+                          .and.returnValue(deferredLogin.promise)           
+        };
+        
+        // mock $state
+        stateMock = jasmine.createSpyObj('$state spy', ['go']);
+        
+        // mock $ionicPopup
+        ionicPopupMock = jasmine.createSpyObj('$ionicPopup spy', ['alert']);
+        
+        // instantiate LoginController
+        controller = $controller('LoginController', { 
+                        '$ionicPopup': ionicPopupMock, 
+                        '$state': stateMock, 
+                        'userService': userServiceMock }
+                     );
+    }));
+    
     describe('#doLogin', function() {
-
-        // TODO: Call doLogin on the Controller
-
-        it('should call login on app', function() {
-            expect(dinnerServiceMock.login).toHaveBeenCalledWith('karmatest@test.com', 'karma'); 
+        
+        // call doLogin on the controller for every test
+        beforeEach(inject(function(_$rootScope_) {
+            $rootScope = _$rootScope_;
+            controller.username = 'test1';
+            controller.password = 'password1';
+            controller.doLogin();
+        }));
+        
+        it('should call login on userService', function() {
+            expect(userServiceMock.login).toHaveBeenCalledWith('karmatest@test.com', 'karma'); 
         });
-
+        
         describe('when the login is executed,', function() {
             it('if successful, should change state to tab.character', function() {
-
-                // TODO: Mock the login response from UserService
-
+                
+                deferredLogin.resolve();
+                $rootScope.$digest();
+                
                 expect(stateMock.go).toHaveBeenCalledWith('tab.character');
             });
-
+            
             it('if unsuccessful, should show a popup', function() {
-
-                // TODO: Mock the login response from UserService
-
+                
+                deferredLogin.reject();
+                $rootScope.$digest();
+                
                 expect(ionicPopupMock.alert).toHaveBeenCalled();
             });
         });
